02aa081b153a0c3cd5c9bfbcfbfeedbcd17e00b3
Felipe Randolfi <fdrandolfi@gmail.com>
2020-11-23 20:59:54 -0300

    add getLabelByNote

--- src/client/structures/notes/index.js
+++ src/client/structures/notes/index.js
@@ -1,4 +1,21 @@
 import NOTES from './notes';
+import NOTES_EXTENDED from './notesExtended';
+
+/**
+ * Returns a label from note id
+ *
+ * @param {Number} noteId the note id
+ * @returns {String} The label of the note
+ */
+const getLabelByNote = (noteId) => {
+  let result = '';
+  NOTES_EXTENDED.forEach((note) => {
+    if (note.id === noteId) {
+      result = note.name;
+    }
+  });
+  return result;
+};
 
 /**
  * Returns an Array with notes
@@ -16,4 +33,4 @@ const getNoteList = () => {
   return resultList;
 };
 
-export default getNoteList;
+export { getLabelByNote, getNoteList };
