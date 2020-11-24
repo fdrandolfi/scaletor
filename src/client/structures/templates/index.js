042523f052425439229671320e420e05e50d9ebf
Felipe Randolfi <fdrandolfi@gmail.com>
2020-11-24 18:49:24 -0300

    add new strandberg boden 7 template

--- src/client/structures/templates/index.js
+++ src/client/structures/templates/index.js
@@ -1,21 +1,28 @@
 import img_G_PRS_C24_6 from '../../assets/images/templates/G-PRS-C24-6.png';
 import img_G_GIBSON_LP_STD_6 from '../../assets/images/templates/G-GIBSON-LP-STD-6.png';
+import img_G_SB_BODEN_7 from '../../assets/images/templates/G-SB-BODEN-7.png';
 
 /**
  * Templates
  */
 const templates = {
+  'G-PRS-C24-6': {
+    label: 'PRS Custom 24',
+    strings: 6,
+    frets: 24,
+    imgSrc: img_G_PRS_C24_6,
+  },
   'G-GIBSON-LP-STD-6': {
     label: 'Gibson Les Paul Standard',
     strings: 6,
     frets: 22,
     imgSrc: img_G_GIBSON_LP_STD_6,
   },
-  'G-PRS-C24-6': {
-    label: 'PRS Custom 24',
-    strings: 6,
+  'G-SB-BODEN-7': {
+    label: 'Strandberg Boden 7',
+    strings: 7,
     frets: 24,
-    imgSrc: img_G_PRS_C24_6,
+    imgSrc: img_G_SB_BODEN_7,
   },
 };
 
